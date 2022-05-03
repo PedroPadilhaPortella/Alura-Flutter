c3421ae072adb4ad5a2af0bca3f131ac620e6bd3
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-05-03 18:29:43 -0300

    trocando rotas componentizadas por rotas nomeadas

--- flutter-nuvigator-start/lib/screens/menu_screen.dart
+++ flutter-nuvigator-start/lib/screens/menu_screen.dart
@@ -2,17 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:proj/components/orgs_menu_card.dart';
 import 'package:proj/components/orgs_rate_app.dart';
 import 'package:proj/core/app_colors.dart';
-import 'package:proj/screens/favorites_screen.dart';
-import 'package:proj/screens/home_screen.dart';
-import 'package:proj/screens/payment_screen.dart';
-import 'package:proj/screens/profile_screen.dart';
-import 'login_screen.dart';
 
 class MenuScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -25,57 +18,37 @@ class MenuScreen extends StatelessWidget {
                 style: TextStyle(
                     fontSize: 26,
                     fontWeight: FontWeight.w700,
-                    color: AppColors.darkGrey
-                ),
+                    color: AppColors.darkGrey),
               ),
               SizedBox(height: 20),
               OrgsRateApp(),
               SizedBox(height: 30),
               OrgsMenuCard(
-                text: 'Início',
-                icon: Icons.home,
-                action: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                ),
-              ),
+                  text: 'Início',
+                  icon: Icons.home,
+                  action: () => Navigator.pushNamed(context, 'home')),
               Divider(),
               OrgsMenuCard(
-                text: 'Favoritos',
-                icon: Icons.favorite,
-                action: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FavoritesScreen()),
-                ),
-              ),
+                  text: 'Favoritos',
+                  icon: Icons.favorite,
+                  action: () => Navigator.pushNamed(context, 'favorites')),
               Divider(),
               OrgsMenuCard(
-                text: 'Perfil',
-                icon: Icons.person,
-                action: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfileScreen()),
-                ),
-              ),
+                  text: 'Perfil',
+                  icon: Icons.person,
+                  action: () => Navigator.pushNamed(context, 'profile')),
               Divider(),
               OrgsMenuCard(
                 text: 'Métodos de pagamento',
                 icon: Icons.credit_card,
-                action: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PaymentScreen()),
-                ),
+                action: () => Navigator.pushNamed(context, 'payment'),
               ),
               Divider(),
               OrgsMenuCard(
-                text: 'Sair',
-                icon: Icons.logout,
-                action: () => Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                  (route) => false
-                ),
-              ),
+                  text: 'Sair',
+                  icon: Icons.logout,
+                  action: () => Navigator.of(context).pushNamedAndRemoveUntil(
+                      'login', (Route<dynamic> route) => false)),
               SizedBox(height: 30),
             ],
           ),
