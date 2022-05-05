89ed06d1e87fe668637e56d29d4205bfecc6a1d4
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-05-05 17:21:35 -0300

    Nested reoutes com Nuvigator

---
+++ Flutter Nuvigator Nested/lib/flows/routes.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:nested_nuvigators/flows/first_flow/screens/one_screen.dart';
+import 'package:nested_nuvigators/flows/first_flow/screens/three_screen.dart';
+import 'package:nested_nuvigators/flows/first_flow/screens/two_screen.dart';
+import 'package:nested_nuvigators/flows/second_flow/screens/display_screen.dart';
+import 'package:nested_nuvigators/flows/second_flow/screens/input_screen.dart';
+import 'package:nuvigator/next.dart';
+
+class _FirstFlowRouter extends NuRouter {
+  @override
+  String get initialRoute => 'first-flow/screen1';
+
+  @override
+  List<NuRoute<NuRouter, Object, Object>> get registerRoutes => [
+        NuRouteBuilder(
+          screenType: materialScreenType,
+          path: 'first-flow/screen1',
+          builder: (_, __, ___) => OneScreen(
+            onNext: () => nuvigator.open('first-flow/screen2'),
+            onClose: () => nuvigator.closeFlow(),
+          ),
+        ),
+        NuRouteBuilder(
+          screenType: materialScreenType,
+          path: 'first-flow/screen2',
+          builder: (_, __, ___) => TwoScreen(
+            onNext: () => nuvigator.open('first-flow/screen3'),
+            onClose: () => nuvigator.closeFlow(),
+          ),
+        ),
+        NuRouteBuilder(
+          screenType: materialScreenType,
+          path: 'first-flow/screen3',
+          builder: (_, __, ___) => ThreeScreen(
+            onNext: () => nuvigator.open('second-flow'),
+            onClose: () => nuvigator.closeFlow(),
+          ),
+        ),
+      ];
+}
+
+class FirstFlowRoute extends NuRoute {
+  @override
+  String get path => 'first-flow';
+
+  @override
+  ScreenType get screenType => materialScreenType;
+
+  @override
+  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
+    return Nuvigator(router: _FirstFlowRouter());
+  }
+}
+
+class _SecondFlowRouter extends NuRouter {
+  @override
+  String get initialRoute => 'second-flow/input';
+
+  @override
+  List<NuRoute<NuRouter, Object, Object>> get registerRoutes => [
+        NuRouteBuilder(
+          screenType: materialScreenType,
+          path: 'second-flow/input',
+          builder: (_, __, ___) => InputScreen(
+            onNext: (text) => nuvigator
+                .open('second-flow/display', parameters: {'text': text}),
+          ),
+        ),
+        NuRouteBuilder(
+          screenType: materialScreenType,
+          path: 'second-flow/display',
+          builder: (_, __, settings) => DisplayScreen(
+            text: settings.rawParameters['text'],
+            onClose: () => nuvigator.closeFlow(),
+          ),
+        ),
+      ];
+}
+
+class SecondFlowRoute extends NuRoute {
+  @override
+  String get path => 'second-flow';
+
+  @override
+  ScreenType get screenType => materialScreenType;
+
+  @override
+  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
+    return Nuvigator(router: _SecondFlowRouter());
+  }
+}
